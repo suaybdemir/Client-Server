4fd8142525b36d33a4cfd3b56b65c3436affbe51
i7260016256 <byausrimed@gmail.com>
2024-08-02 21:49:46 +0300

    .

--- client.cpp
+++ client.cpp
@@ -1,51 +1,114 @@
 #include <iostream>
 #include <string>
+#include <thread>
+#include <mutex>
+#include <cstdlib>
 #include <cstring>
 #include <unistd.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
+ #include <stdarg.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
+#define BUF_SIZE 1024
+#define SERVER_PORT 8081 
+#define IP "127.0.0.1"
 
-int main() {
-    int sock = 0;
+void send_msg(int sock);
+void recv_msg(int sock);
+int output(const char *arg,...);
+int error_output(const char *arg,...);
+void error_handling(const std::string &message);
+
+std::string name = "DEFAULT";
+std::string msg;
+
+int main(int argc,const char **argv,const char **envp){
+    int sock;
+    // sockaddr_in serv_addr{};
     struct sockaddr_in serv_addr;
-    char buffer[BUFFER_SIZE] = {0};
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Socket creation error" << std::endl;
-        return -1;
+    if (argc != 2){
+        error_output("Usage : %s <Name> \n",argv[0]);
+        exit(1);
     }
 
+
+    name = "["+std::string(argv[1])+"]";
+
+    // sock=socket(PF_INET, SOCK_STREAM, 0);
+
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == -1){
+        error_handling("socket() failed!");
+    }
+
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        std::cerr << "Invalid address or address not supported" << std::endl;
-        return -1;
+    serv_addr.sin_addr.s_addr = inet_addr(IP);
+    serv_addr.sin_port = htons(SERVER_PORT);
+    
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1){
+        error_handling("connect() failed!");
     }
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection failed" << std::endl;
-        return -1;
-    }
-
-    while (true) {
-        std::string message;
-        std::cout << "You: ";
-        std::getline(std::cin, message);
-        send(sock, message.c_str(), message.size(), 0);
-
-        memset(buffer, 0, BUFFER_SIZE);
-        int bytesRead = read(sock, buffer, BUFFER_SIZE);
-        if (bytesRead == 0) {
-            std::cout << "Server disconnected" << std::endl;
-            break;
-        }
-        std::cout << "Server: " << buffer << std::endl;
-    }
-
+ 
+    std::string my_name = "#new client:" + std::string(argv[1]);
+    send(sock, my_name.c_str(), my_name.length() + 1, 0);
+    
+    std::thread snd(send_msg, sock);
+    std::thread rcv(recv_msg, sock);
+    
+    snd.join();
+    rcv.join();
+    
     close(sock);
+
     return 0;
 }
 
+void send_msg(int sock){
+    while(1){
+        getline(std::cin, msg);
+        if (msg == "Quit"|| msg == "quit"){
+            close(sock);
+            exit(0);
+        }
+     
+        std::string name_msg = name + " " + msg;
+        send(sock, name_msg.c_str(), name_msg.length() + 1, 0);
+    }
+}
+
+void recv_msg(int sock){
+    char name_msg[BUF_SIZE + name.length() + 1];
+    while (1){
+        int str_len = recv(sock, name_msg, BUF_SIZE+name.length() + 1, 0);
+        if (str_len == -1){
+            exit(-1);
+        }
+        std::cout<<std::string(name_msg)<<std::endl;
+    }
+}
+
+int output(const char *arg, ...){
+    int res;
+    va_list ap;
+    va_start(ap, arg);
+    res = vfprintf(stdout, arg, ap);
+    va_end(ap);
+    return res;
+}
+
+int error_output(const char *arg, ...){
+    int res;
+    va_list ap;
+    va_start(ap, arg);
+    res = vfprintf(stderr, arg, ap);
+    va_end(ap);
+    return res;
+}
+
+void error_handling(const std::string &message){
+    std::cerr<<message<<std::endl;
+    exit(1);
+}
--- server.cpp
+++ server.cpp
@@ -1,81 +1,184 @@
 #include <iostream>
-#include <string>
 #include <cstring>
+#include <thread>
+#include <mutex>
+#include <cstdlib>
 #include <unistd.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unordered_map>
+ #include <stdarg.h>
 
-#define PORT 8081
-#define BUFFER_SIZE 1024
+#define SERVER_PORT 8081
+#define BUF_SIZE 1024
+#define MAX_CLNT 256   
 
-void handleClient(int clientSocket) {
-    char buffer[BUFFER_SIZE] = {0};
-    while (true) {
-        memset(buffer, 0, BUFFER_SIZE);
-        int bytesRead = read(clientSocket, buffer, BUFFER_SIZE);
-        if (bytesRead == 0) {
-            std::cout << "Client disconnected" << std::endl;
-            break;
-        }
-        std::cout << "Client: " << buffer << std::endl;
-        std::string message;
-        std::getline(std::cin, message);
-        send(clientSocket, message.c_str(), message.size(), 0);
-    }
-    close(clientSocket);
-}
+void handle_clnt(int clnt_sock);
+void send_msg(const std::string &msg);
+int output(const char *arg,...);
+int error_output(const char *arg,...);
+void error_handling(const std::string &message);
 
-int main() {
-    int serverFd, newSocket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
+int clnt_cnt = 0;
+std::mutex mtx;
+std::unordered_map<std::string, int>clnt_socks;
 
-    // Create socket file descriptor
-    if ((serverFd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+int main(int argc,const char **argv,const char **envp){
+    int serv_sock,clnt_sock;
+    struct sockaddr_in serv_addr, clnt_addr;
+    socklen_t clnt_addr_size;
+
+    serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serv_sock == -1){
+        error_handling("socket() failed!");
     }
 
-    // Set the SO_REUSEADDR socket option
-    if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt failed");
-        close(serverFd);
-        exit(EXIT_FAILURE);
+    memset(&serv_addr,0, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    // serv_addr.sin_port=htons(atoi(argv[1]));
+    serv_addr.sin_port = htons(SERVER_PORT);
+
+
+    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1){
+        error_handling("bind() failed!");
+    }
+    printf("the server is running on port %d\n", SERVER_PORT);
+
+    if (listen(serv_sock, MAX_CLNT) == -1){
+        error_handling("listen() error!");
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // Bind the socket to the address and port
-    if (bind(serverFd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        close(serverFd);
-        exit(EXIT_FAILURE);
-    }
-
-    // Listen for incoming connections
-    if (listen(serverFd, 3) < 0) {
-        perror("listen failed");
-        close(serverFd);
-        exit(EXIT_FAILURE);
-    }
-
-    std::cout << "Server is listening on port " << PORT << std::endl;
-
-    while (true) {
-        // Accept an incoming connection
-        if ((newSocket = accept(serverFd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept failed");
-            close(serverFd);
-            exit(EXIT_FAILURE);
+    while(true){  
+        clnt_addr_size = sizeof(clnt_addr);
+       
+        clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+        if (clnt_sock == -1){
+            error_handling("accept() failed!");
         }
 
-        std::cout << "Connection established with client" << std::endl;
-        handleClient(newSocket);
-    }
+    
+        mtx.lock();
+        clnt_cnt++;
+        mtx.unlock();
 
-    close(serverFd);
+
+        std::thread th(handle_clnt, clnt_sock);
+        th.detach();
+
+        output("Connected client IP: %s \n", inet_ntoa(clnt_addr.sin_addr));
+    }
+    close(serv_sock);
     return 0;
 }
 
+void handle_clnt(int clnt_sock){
+    char msg[BUF_SIZE];
+    int flag = 0;
+
+    char tell_name[13] = "#new client:";
+    while(recv(clnt_sock, msg, sizeof(msg),0) != 0){
+
+        if (std::strlen(msg) > std::strlen(tell_name)) {
+     
+            char pre_name[13];
+            std::strncpy(pre_name, msg, 12);
+            pre_name[12] = '\0';
+            if (std::strcmp(pre_name, tell_name) == 0) {
+         
+                char name[20];
+                std::strcpy(name, msg+12);
+                if(clnt_socks.find(name) == clnt_socks.end()){
+                    output("the name of socket %d: %s\n", clnt_sock, name);
+                    clnt_socks[name] = clnt_sock;
+                }
+                else {
+                 
+                    std::string error_msg = std::string(name) + " exists already. Please quit and enter with another name!";
+                    send(clnt_sock, error_msg.c_str(), error_msg.length()+1, 0);
+                    mtx.lock();
+                    clnt_cnt--;
+                    mtx.unlock();
+                    flag = 1;
+                }
+            }
+        }
+
+        if(flag == 0)
+            send_msg(std::string(msg));
+    }
+    if(flag == 0){
+    
+        std::string leave_msg;
+        std::string name;
+        mtx.lock();
+        for (auto it = clnt_socks.begin(); it != clnt_socks.end(); ++it ){
+            if(it->second == clnt_sock){
+                name = it->first;
+                clnt_socks.erase(it->first);
+            }
+        }
+        clnt_cnt--;
+        mtx.unlock();
+        leave_msg = "client " + name + " leaves the chat room";
+        send_msg(leave_msg);
+        output("client %s leaves the chat room\n", name.c_str());
+        close(clnt_sock);
+    }
+    else {
+        close(clnt_sock);
+    }
+}
+
+void send_msg(const std::string &msg){
+    mtx.lock();
+
+    std::string pre = "@";
+    int first_space = msg.find_first_of(" ");
+    if (msg.compare(first_space+1, 1, pre) == 0){
+
+        int space = msg.find_first_of(" ", first_space+1);
+        std::string receive_name = msg.substr(first_space+2, space-first_space-2);
+        std::string send_name = msg.substr(1, first_space-2);
+        if(clnt_socks.find(receive_name) == clnt_socks.end()) {
+
+            std::string error_msg = "[error] there is no client named " + receive_name;
+            send(clnt_socks[send_name], error_msg.c_str(), error_msg.length()+1, 0);
+        }
+        else {
+            send(clnt_socks[receive_name], msg.c_str(), msg.length()+1, 0);
+            send(clnt_socks[send_name], msg.c_str(), msg.length()+1, 0);
+        }
+    }
+    else {
+    
+        for (auto it = clnt_socks.begin(); it != clnt_socks.end(); it++) {
+            send(it->second, msg.c_str(), msg.length()+1, 0);
+        }
+    }
+    mtx.unlock();
+}
+
+int output(const char *arg, ...){
+    int res;
+    va_list ap;
+    va_start(ap, arg);
+    res = vfprintf(stdout, arg, ap);
+    va_end(ap);
+    return res;
+}
+
+int error_output(const char *arg, ...){
+    int res;
+    va_list ap;
+    va_start(ap, arg);
+    res = vfprintf(stderr, arg, ap);
+    va_end(ap);
+    return res;
+}
+
+void error_handling(const std::string &message){
+    std::cerr<<message<<std::endl;
+    exit(1);
+}
